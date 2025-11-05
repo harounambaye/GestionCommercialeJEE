96dd878e244e5b30ecad6c12aba74a0cf9463131
Harouna MBAYE <roonmbaye5@gmail.com>
2025-11-05 01:04:38 +0000

    Delete ProjetJEE25V1 directory

--- ProjetJEE25V1/sama_boutik_ejb/ejbModule/dao/UtilisateurDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entity.Personne;
-import java.util.List;
-
-public interface UtilisateurDao {
-    List<Personne> findAll();
-    List<Personne> findByProfil(Long profilId);
-}
